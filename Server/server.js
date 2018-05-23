d483934c5b3ee12c869f727f6754ca880a5b7110
Alix Goguey <alix.goguey@gmail.com>
2018-05-23 23:20:26 +0200

    adding and removing cubes from tile configuration

--- Server/server.js
+++ Server/server.js
@@ -15,17 +15,210 @@ var port = 9000;
 http.listen(port);
 datelog('Server listening on port '+port+'.')
 
-var list_watch_client = {}
+var list_watch_client = {};
+var qt_client = null;
+
+var buffer_qt = null;
+var side_msgs = [];
+var configuration = {'devices':[], 'watches': []};
+// {'devices': [ {'min_x':-1, 'max_x': 0, 'min_y':0, 'max_y':0, 'min_z':0, 'max_z':0,
+//                'connections': {
+//                                 id1: {'x':0, 'y':0, 'z':0, 'B':null, 'T':null, 'N':null, 'S':null, 'E':null, 'W':id2},
+//                                 id2: {'x':-1, 'y':0, 'z':0, 'B':null, 'T':null, 'N':null, 'S':null, 'E':id1, 'W':null}
+//                               }
+//               }
+//               {'min_x':-1, 'max_x': 0, 'min_y':0, 'max_y':1, 'min_z':0, 'max_z':0,
+//                'connections': {
+//                                 id1: {'x':0, 'y':0, 'z':0, 'B':null, 'T':null, 'N':null, 'S':null, 'E':null, 'W':id2},
+//                                 id2: {'x':-1, 'y':0, 'z':0, 'B':id3, 'T':null, 'N':null, 'S':null, 'E':id1, 'W':null},
+//                                 id3: {'x':-1, 'y':1, 'z':0, 'B':null, 'T':id2, 'N':null, 'S':null, 'E':id1, 'W':null}
+//                               }
+//               }
+// 
+// 
+//             ],
+//  'watches': [id1, id2, id3]
+// }
+
+function print_configuration() {
+  datelog("Configuration");
+  datelog("  Watches");
+  datelog("      -> "+configuration['watches']);
+  datelog("  Devices");
+  for (var i = 0; i < configuration['devices'].length; i++) {
+    datelog("      -> min_x "+configuration['devices'][i]['min_x']+"   max_x "+configuration['devices'][i]['max_x']);
+    datelog("         min_y "+configuration['devices'][i]['min_y']+"   max_y "+configuration['devices'][i]['max_y']);
+    datelog("         min_z "+configuration['devices'][i]['min_z']+"   max_z "+configuration['devices'][i]['max_z']);
+    datelog("         Connections");
+    for (var key in configuration['devices'][i]['connections']) {
+      var str = "             -> ";
+      str += "id "+key;
+      str += ", x "+configuration['devices'][i]['connections'][key]['x'];
+      str += ", y "+configuration['devices'][i]['connections'][key]['y'];
+      str += ", z "+configuration['devices'][i]['connections'][key]['z'];
+      var con = ['B', 'T', 'N', 'S', 'W', 'E'];
+      for (var k = 0; k < con.length; k++) {
+        str += ", "+con[k] + ": "+configuration['devices'][i]['connections'][key][con[k]];
+      }
+      datelog(str);
+    }
+  }
+}
+
+function update_device(k) {
+  // device k might not be continuous anymore
+  var keys = [];
+  for (var key in configuration['devices'][k]['connections']) {
+    keys.push(key);
+  }
+  if (keys.length == 0) {
+    configuration['devices'].splice(k, 1);
+    return;
+  }
+
+  var devices = [];
+  while (keys.length > 0) {
+    devices.push({});
+    var in_process = [keys.pop()];
+    while (in_process.length > 0) {
+      var key = in_process.pop();
+      devices[devices.length-1][key] = configuration['devices'][k]['connections'][key];
+
+      var con = ['B', 'T', 'N', 'S', 'W', 'E'];
+      for (var i = 0; i < con.length; i++) {
+        if (configuration['devices'][k]['connections'][key][con[i]] != null) {
+          in_process.push();
+          var index = keys.indexOf(configuration['devices'][k]['connections'][key][con[i]]);
+          if (index > -1) {
+            keys.splice(index, 1);
+            in_process.push(configuration['devices'][k]['connections'][key][con[i]]);
+          }
+        }
+      }
+    }
+  }
+
+  // check for the geometry
+  configuration['devices'].splice(k, 1);
+  for (var i = devices.length-1; i >= 0; i--) {
+    var device = {'connections': devices[i]};
+    var once = false;
+    for (var con in device['connections']) {
+      if (!once) {
+        device['min_x'] = device['connections'][con]['x'];
+        device['max_x'] = device['connections'][con]['x'];
+        device['min_y'] = device['connections'][con]['y'];
+        device['max_y'] = device['connections'][con]['y'];
+        device['min_z'] = device['connections'][con]['z'];
+        device['max_z'] = device['connections'][con]['z'];
+        once = true;
+      } else {
+        device['min_x'] = min(device['connections'][con]['x'],device['min_x']);
+        device['max_x'] = max(device['connections'][con]['x'],device['max_x']);
+        device['min_y'] = min(device['connections'][con]['y'],device['min_y']);
+        device['max_y'] = max(device['connections'][con]['y'],device['max_y']);
+        device['min_z'] = min(device['connections'][con]['z'],device['min_z']);
+        device['max_z'] = max(device['connections'][con]['z'],device['max_z']);
+      }
+    }
+    configuration['devices'].splice(k, 0, device);
+  }
+}
+
+function update_configuration(connection,watch) {
+  if (watch != null)  {
+    to_remove = null;
+    to_add = null;
+
+    if (!connection) {
+      var index = configuration['watches'].indexOf(watch);
+      if (index > -1) {
+        to_remove = watch;
+        configuration['watches'].splice(index, 1);
+      }
+    } else {
+      var index = configuration['watches'].indexOf(watch);
+      if (index > -1) {
+        print_configuration();
+        return;
+      }
+      to_add = watch;
+      configuration['watches'].push(watch)
+    }
+
+    if (to_remove != null) {
+      // remove watch
+      for (var k = 0; k < configuration['devices'].length; k++) {
+        if (to_remove in configuration['devices'][k]['connections']) {
+          if (configuration['devices'][k]['connections'][to_remove]['B'] != null) {
+            configuration['devices'][k]['connections'][configuration['devices'][k]['connections'][to_remove]['B']]['T'] = null;
+          }
+          if (configuration['devices'][k]['connections'][to_remove]['T'] != null) {
+            configuration['devices'][k]['connections'][configuration['devices'][k]['connections'][to_remove]['T']]['B'] = null;
+          }
+          if (configuration['devices'][k]['connections'][to_remove]['N'] != null) {
+            configuration['devices'][k]['connections'][configuration['devices'][k]['connections'][to_remove]['N']]['S'] = null;
+          }
+          if (configuration['devices'][k]['connections'][to_remove]['S'] != null) {
+            configuration['devices'][k]['connections'][configuration['devices'][k]['connections'][to_remove]['S']]['N'] = null;
+          }
+          if (configuration['devices'][k]['connections'][to_remove]['W'] != null) {
+            configuration['devices'][k]['connections'][configuration['devices'][k]['connections'][to_remove]['W']]['E'] = null;
+          }
+          if (configuration['devices'][k]['connections'][to_remove]['E'] != null) {
+            configuration['devices'][k]['connections'][configuration['devices'][k]['connections'][to_remove]['E']]['W'] = null;
+          }
+          delete configuration['devices'][k]['connections'][to_remove];
+          update_device(k);
+          break;
+        }
+      }
+    } else if (to_add != null) {
+      // add new device as a new device
+      device = {'min_x':0, 'max_x': 0, 'min_y':0, 'max_y':0, 'min_z':0, 'max_z':0, 'connections': {}};
+      device['connections'][to_add] = {'x':0, 'y':0, 'z':0, 'B':null, 'T':null, 'N':null, 'S':null, 'E':null, 'W':null};
+      configuration['devices'].push(device);
+    }
+  } else {
+    // add / remove configurations
+
+    // data["IMEI"]
+    // data["side"]   // "North" "East" "South" "West" "Bottom"
+    // data["active"] // "false" "true"
+  }
+
+  print_configuration();
+}
+
+function send_configuration_qt() {
+  update_configuration(true,null);
+  if (qt_client != null) {
+    qt_client.write("conf:"+configuration);
+  }
+  buffer_qt = null;
+}
+
+function handle_side(message) {
+  if (buffer_qt != null) {
+    clearTimeout(buffer_qt);
+  }
+  side_msgs.push(message);
+  buffer_qt = setTimeout(send_configuration_qt, 100);
+}
 
 io.on('connection', function(socket){
   datelog("New client connected");
 
-  list_watch_client[socket.id] = socket
+  list_watch_client[socket.id] = {'socket': socket}
 
   socket.emit('event', {"id": socket.id});
 
   socket.on('connected', function(data) {
       datelog("Client ("+data["IMEI"]+") says: "+data["Hi"]);
+      list_watch_client[socket.id]['IMEI'] = data["IMEI"];
+      list_watch_client[data["IMEI"]] = {'socket': socket.id };
+
+      update_configuration(true,data["IMEI"]);
   });
 
   socket.on('clicked', function(data) {
@@ -41,13 +234,19 @@ io.on('connection', function(socket){
     
   socket.on('sideChange', function(data) {
       datelog("Client ("+data["IMEI"]+") says: sideChange!");
-      datelog("Client ("+data["IMEI"]+")         side: "+data["side"]);
-      datelog("Client ("+data["IMEI"]+")         active: "+data["active"]);
+      datelog("Client ("+data["IMEI"]+")        side: "+data["side"]);   // "North" "East" "South" "West" "Bottom"
+      datelog("Client ("+data["IMEI"]+")      active: "+data["active"]); // "false" "true"
+
+      handle_side(data);
   });
 
   socket.on('disconnect' , function() {
       datelog("Client disconnected");
-      delete list_watch_client[socket.id]
+
+      update_configuration(false,list_watch_client[socket.id]['IMEI']);
+
+      delete list_watch_client[list_watch_client[socket.id]['IMEI']];
+      delete list_watch_client[socket.id];
   });
 });
 
@@ -64,6 +263,7 @@ function onClientConnected(socket) {
 
   var clientName = 'Qt Client'
   datelog(clientName + ' connected.');
+  qt_client = socket;
 
   socket.on('data', (data) => { 
     //var msg = data.toString().replace(/[\n\r]*$/, '');
@@ -74,6 +274,7 @@ function onClientConnected(socket) {
     // notifing the client
     // socket.write('We got your message (' + msg + '). Thanks!\n');
     socket.write('We got your message. Thanks!\n');
+    send_configuration_qt();
 
     for (var key in list_watch_client) {
         list_watch_client[key].emit('qt', {"msg": data});
@@ -82,6 +283,7 @@ function onClientConnected(socket) {
   
   socket.on('end', () => {
     datelog(clientName + ' disconnected.');
+    qt_client = null;
   });
 }
 
